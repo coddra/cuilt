4bf4bcdf3d366446a70536623415c59d7d2fc20a
Coddra <andrew.reeds.mpg@gmail.com>
2024-06-18 17:51:20 +0200

    Add log level fatal

--- cuilt.h
+++ cuilt.h
@@ -15,10 +15,10 @@
 #include <unistd.h>
 #endif
 
-#define ARG_COUNT(...) __ARG_COUNT(__VA_ARGS__, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, \
+#define ARG_COUNT(...) ___ARG_COUNT(__VA_ARGS__, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, \
     46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, \
     18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
-#define __ARG_COUNT(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, \
+#define ___ARG_COUNT(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, \
     _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43,   \
     _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, N, ...) N
 
@@ -42,23 +42,33 @@ enum LOG_LEVEL {
     LOG_INFO = 0,
     LOG_WARN = 1,
     LOG_ERROR = 2,
+    LOG_FATAL = 3,
 };
 
 enum LOG_LEVEL log_level = LOG_WARN;
 
-void msg(int level, char *fmt, ...) {
+void msg(enum LOG_LEVEL level, char *fmt, ...) {
     if (level < log_level)
         return;
+    
     switch (level) {
         case LOG_INFO: printf("[INF] "); break;
         case LOG_WARN: printf("[WRN] "); break;
         case LOG_ERROR: printf("[ERR] "); break;
+        case LOG_FATAL: printf("[FTL] "); break;
     }
+
     va_list ap;
     va_start(ap, fmt);
     vprintf(fmt, ap);
     va_end(ap);
     printf("\n");
+
+    if (level == LOG_FATAL) {
+        fflush(stdout);
+        fflush(stderr);
+        exit(1);
+    }
 }
 
 #define INFO(...) msg(LOG_INFO, __VA_ARGS__)
